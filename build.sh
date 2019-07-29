0f43ce3f5a1a7109221b4cafe3799ff576f5e7c1
Knut Ahlers <knut@ahlers.me>
2019-07-29 09:57:20 +0200

    Pin coredns, they fucked up again
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- build.sh
+++ build.sh
@@ -5,15 +5,16 @@ set -euxo pipefail
 apk --no-cache add curl
 
 # Get latest versions of tools using latestver
-COREDNS_VERSION=$(curl -sSfL 'https://lv.luzifer.io/catalog-api/coredns/latest.txt?p=version')
+#COREDNS_VERSION=$(curl -sSfL 'https://lv.luzifer.io/catalog-api/coredns/latest.txt?p=version')
+COREDNS_VERSION=1.5.2
 DUMB_INIT_VERSION=$(curl -sSfL 'https://lv.luzifer.io/catalog-api/dumb-init/latest.txt?p=version')
 
 [ -z "${COREDNS_VERSION}" ] && { exit 1; }
 [ -z "${DUMB_INIT_VERSION}" ] && { exit 1; }
 
 # Install tools
-curl -sSfL https://github.com/coredns/coredns/releases/download/v${COREDNS_VERSION}/coredns_${COREDNS_VERSION}_linux_amd64.tgz | \
-  tar -x -z -C /usr/local/bin
+curl -sSfL https://github.com/coredns/coredns/releases/download/v${COREDNS_VERSION}/coredns_${COREDNS_VERSION}_linux_amd64.tgz |
+	tar -x -z -C /usr/local/bin
 
 curl -sSfLo /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v${DUMB_INIT_VERSION}/dumb-init_${DUMB_INIT_VERSION}_amd64
 chmod +x /usr/local/bin/dumb-init
