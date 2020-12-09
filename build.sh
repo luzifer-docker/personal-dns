1c74e3298db99b89c5e03bf91dbe518ed6928b2c
Knut Ahlers <knut@ahlers.me>
2020-12-09 23:06:11 +0100

    Update filename of dumb-init build
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ DUMB_INIT_VERSION=$(curl -sSfL 'https://lv.luzifer.io/catalog-api/dumb-init/late
 [ -z "${DUMB_INIT_VERSION}" ] && { exit 1; }
 
 # Install tools
-curl -sSfLo /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v${DUMB_INIT_VERSION}/dumb-init_${DUMB_INIT_VERSION}_amd64
+curl -sSfLo /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v${DUMB_INIT_VERSION}/dumb-init_${DUMB_INIT_VERSION}_x86_64
 chmod +x /usr/local/bin/dumb-init
 
 # Cleanup
